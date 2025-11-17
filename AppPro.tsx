25ddb80d0d2e747bf5ff63a7cfe349d10075f4e1
yashPundhir <yash.pundhir.prof@gmail.com>
2025-11-17 23:58:04 +0530

    use AppPro as the start point of app

---
+++ AppPro.tsx
@@ -0,0 +1,25 @@
+import React, { JSX } from 'react';
+
+import { View, Text, StyleSheet, useColorScheme } from 'react-native';
+
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+function AppPro(): JSX.Element {
+  return (
+    <SafeAreaProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ color: '#40c2ba' }}>Basant Chauhan</Text>
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+// const styles = StyleSheet.
+
+export default AppPro;
